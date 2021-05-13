60094013e7143068d4a1e1a63d9674c02ffe2ca9
Anders429 <anders.stefan.evensen@gmail.com>
2021-05-12 21:54:05 -0700

    Unnest imports on doctest.

--- build.rs
+++ build.rs
@@ -5,11 +5,4 @@ fn main() {
 
     ac.emit_has_type("u128");
     ac.emit_has_type("i128");
-
-    #[cfg(test)]
-    {
-        if ac.probe_rustc_version(1, 25) {
-            autocfg::emit("use_nested_groups");
-        }
-    }
 }

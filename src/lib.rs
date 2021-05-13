c7f69fadf554920c34a02b11a806b52db8a7b263
Anders429 <anders.stefan.evensen@gmail.com>
2021-05-12 17:20:54 -0700

    Documentation of crate and struct.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! Provides an [`IdentityHasher`] which passes on the value it is given.
+//!
+//! This is an incredibly naive hasher. It is not cryptographically secure, it does not prevent DoS
+//! attacks, and it can only be used to write a single value of up to 8 bytes. Its main use case is
+//! internally on primitive values.
+//!
+//! An example use case is to store [`u64`]s in a [`HashSet`]. This is done as follows:
+//!
+//! ```
+//! use identity_hasher::IdentityHasher;
+//! use std::{collections::HashSet, hash::BuildHasherDefault};
+//!
+//! let mut set = HashSet::with_hasher(BuildHasherDefault::<IdentityHasher>::default());
+//! set.insert(42);
+//! set.insert(100);
+//!
+//! assert_eq!(set.len(), 2);
+//! ```
+
 #![no_std]
 
 #[cfg(feature = "doc_item")]
@@ -14,6 +33,38 @@ mod impl_serde;
 #[cfg(feature = "doc_item")]
 use doc_item::since;
 
+/// A hasher which passes on the value it is given.
+///
+/// This hasher acts as an identity function, returning exactly what is written to it. Consequently,
+/// this hasher can only be written to a single time. Additionally, no more than 8 bytes may be
+/// written to it since the output of a hasher must be 8 bytes.
+///
+/// Incorrect usage of this hasher will panic on debug builds, but the panics will be optimized away
+/// on release builds. Incorrect usage isn't undefined behavior, but it will not function as
+/// expected.
+///
+/// # Examples
+/// ```
+/// use identity_hasher::IdentityHasher;
+/// use std::hash::Hasher;
+///
+/// let mut hasher = IdentityHasher::default();
+/// hasher.write_u64(42);
+///
+/// assert_eq!(hasher.finish(), 42);
+/// ```
+///
+/// Note that writing multiple times to the hasher is incorrect usage. The following will panic on
+/// debug builds and is considered incorrect.
+///
+/// ``` no_run
+/// use identity_hasher::IdentityHasher;
+/// use std::hash::Hasher;
+///
+/// let mut hasher = IdentityHasher::default();
+/// hasher.write_u64(42);
+/// hasher.write_u32(100);  // Can't write twice!
+/// ```
 #[cfg_attr(feature = "doc_item", since(content = "1.8.0"))]
 #[derive(Clone, Debug, Default, PartialEq, Eq)]
 pub struct IdentityHasher {
